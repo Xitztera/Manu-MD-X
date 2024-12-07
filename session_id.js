d6acfe38943e9d282c65d0dd345b3f348f645c6e
𝚇_𝙸𝚃𝚉_𝚃𝙴𝚁𝙰_ <148376006+Xitztera@users.noreply.github.com>
2024-12-07 18:31:39 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&aYUBEZZI#ZgGcRmL8mrkR51HwcxhWJ_rG_doDjG6k1hYivX14b4o",
 };
